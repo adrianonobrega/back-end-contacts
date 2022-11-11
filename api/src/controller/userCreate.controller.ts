54996ee1c60b49afabf3be2cdfe19456d7399311
adrianonobrega <adrianonobrega26@gmail.com>
2022-11-11 16:30:06 -0300

    user login

--- api/src/controller/userCreate.controller.ts
+++ api/src/controller/userCreate.controller.ts
@@ -4,7 +4,7 @@ import {userListService} from "../services/user/userList.services"
 // import { userOneListService } from "../services/user/userOneList.services"
 // import { userUpdateService } from "../services/user/userUpdate.services"
 import { userDeleteService } from "../services/user/userDelete.services"
-// import { userLoginServices } from "../services/user/userLogin.services"
+import { userLoginServices } from "../services/user/userLogin.services"
 
 const userCreateController = async (req: Request, res: Response) => {
     
@@ -97,26 +97,24 @@ const userCreateController = async (req: Request, res: Response) => {
       }
   }
 
-//   const userLoginController = async (req: Request, res: Response) => {
+  const userLoginController = async (req: Request, res: Response) => {
   
-//       try{
-     
-            
-//              const {email,password} = req.body
+      try{
+             const {email,password} = req.body
          
-//          const token = await userLoginServices({email,password})
+         const token = await userLoginServices({email,password})
      
-//          res.status(201).json(token)
-//          }
+         res.status(201).json(token)
+         }
      
-//       catch(error){
-//          if(error instanceof Error){
-//              return res.status(400).json({
-//                  message: error.message
-//                  })
-//              }
-//          }
-//      }
+      catch(error){
+         if(error instanceof Error){
+             return res.status(400).json({
+                 message: error.message
+                 })
+             }
+         }
+     }
 
 
-   export{userCreateController,userDeleteController,userListController} 
+   export{userCreateController,userDeleteController,userListController,userLoginController} 
--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -4,13 +4,13 @@ import { userListController } from "../controller/userCreate.controller";
 // import { userListOneController } from "../controller/user.controllers";
 // import { userUpdateController } from "../controller/user.controllers";
 import { userDeleteController } from "../controller/userCreate.controller";
-// import { userLoginController} from "../controller/user.controllers";
+import { userLoginController} from "../controller/userCreate.controller";
 // import  {authUser}  from "../middlewares/authUser.middleware";
 
 export const userRoutes = Router()
 
 userRoutes.post("/",userCreateController)
-// userRoutes.post("/login",userLoginController)
+userRoutes.post("/login",userLoginController)
 userRoutes.get("/",userListController)
 // userRoutes.patch("/:id",authUser,userUpdateController)
 userRoutes.delete("/:id",userDeleteController)
