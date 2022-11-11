ff2e1e6dc3c650894f9f863ba579f75f551ca774
adrianonobrega <adrianonobrega26@gmail.com>
2022-11-11 16:19:51 -0300

    delete user

--- api/src/controller/userCreate.controller.ts
+++ api/src/controller/userCreate.controller.ts
@@ -3,7 +3,7 @@ import { userCreateServices } from "../services/user/userCreate.services"
 // import {userListService} from "../services/user/userList.services"
 // import { userOneListService } from "../services/user/userOneList.services"
 // import { userUpdateService } from "../services/user/userUpdate.services"
-// import { userDeleteService } from "../services/user/userDelete.services"
+import { userDeleteService } from "../services/user/userDelete.services"
 // import { userLoginServices } from "../services/user/userLogin.services"
 
 const userCreateController = async (req: Request, res: Response) => {
@@ -23,5 +23,100 @@ const userCreateController = async (req: Request, res: Response) => {
        }
    }
 
+//    const userListController = async (req: Request, res: Response) => {
+   
+//     try{
+//      const users = await userListService()
+ 
+//      res.status(200).json(users)
+//     }
+ 
+//      catch(error){
+//          if(error instanceof Error){
+//              return res.status(400).json({
+//                  message: error.message
+//              })
+//          }
+//      }
+//  }
+ 
+//  const userListOneController = async (req: Request, res: Response) => {
+ 
+//      const {id} = req.params
+//      try{
+//       const users = await userOneListService(id)
+  
+//       res.status(200).json(users)
+//      }
+  
+//       catch(error){
+//           if(error instanceof Error){
+//               return res.status(400).json({
+//                   message: error.message
+//               })
+//           }
+//       }
+//   }
+ 
+//   const userUpdateController = async (req: Request, res: Response) => {
+     
+//      try{
+    
+//           const {id} = req.params  
+//           const {email,phone,password} = req.body
+        
+//         const newUser = await userUpdateService({id,email,phone,password})
+    
+//         res.status(201).json(newUser)
+//         }
+    
+//      catch(error){
+//         if(error instanceof Error){
+//             return res.status(400).json({
+//                 message: error.message
+//                 })
+//             }
+//         }
+//     }
+ 
+    const userDeleteController = async (req: Request, res: Response) => {
+ 
+     const {id} = req.params
+     try{
+      const users = await userDeleteService(id)
+  
+      res.status(204).json(users)
+     }
+  
+      catch(error){
+          if(error instanceof Error){
+              return res.status(400).json({
+                  message: error.message
+              })
+          }
+      }
+  }
 
-   export{userCreateController} 
+//   const userLoginController = async (req: Request, res: Response) => {
+  
+//       try{
+     
+            
+//              const {email,password} = req.body
+         
+//          const token = await userLoginServices({email,password})
+     
+//          res.status(201).json(token)
+//          }
+     
+//       catch(error){
+//          if(error instanceof Error){
+//              return res.status(400).json({
+//                  message: error.message
+//                  })
+//              }
+//          }
+//      }
+
+
+   export{userCreateController,userDeleteController} 
--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -3,7 +3,7 @@ import { userCreateController } from "../controller/userCreate.controller";
 // import { userListController } from "../controller/user.controllers";
 // import { userListOneController } from "../controller/user.controllers";
 // import { userUpdateController } from "../controller/user.controllers";
-// import { userDeleteController } from "../controller/user.controllers";
+import { userDeleteController } from "../controller/userCreate.controller";
 // import { userLoginController} from "../controller/user.controllers";
 // import  {authUser}  from "../middlewares/authUser.middleware";
 
@@ -13,5 +13,5 @@ userRoutes.post("/",userCreateController)
 // userRoutes.post("/login",userLoginController)
 // userRoutes.get("/",authUser,userListController)
 // userRoutes.patch("/:id",authUser,userUpdateController)
-// userRoutes.delete("/:id",authUser,userDeleteController)
+userRoutes.delete("/:id",userDeleteController)
 // userRoutes.get("/:id",authUser,userListOneController)
